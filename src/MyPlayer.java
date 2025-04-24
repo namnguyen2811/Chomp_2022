ba196d3d2fc3b9d79f95771cb3f93f85e9679345
namanhnguyen <nam_nguyen25@milton.edu>
2025-04-24 12:05:44 -0400

    chomps all possible 3x3 boards

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -11,6 +11,16 @@ public class MyPlayer {
          * This code will run just once, when the game opens.
          * Add your code here.
          */
+
+        for(int i = 1; i<4; i++){
+            for(int j =0; j<4; j++){
+                for(int k = 0; k<4; k++){
+                    if(i >= j && j >= k){
+                        System.out.println(i + " " + j + " " + k);
+                    }
+                }
+            }
+        }
     }
 
     public Point move(Chip[][] pBoard) {
